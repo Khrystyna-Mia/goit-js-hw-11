64b478d7669336d677f7824f9fbff26f674d4eaa
Khrystyna Hrekul <grekulkristi@gmail.com>
2022-02-19 21:58:29 +0200

    removed loading

--- src/js/api/get-refs.js
+++ src/js/api/get-refs.js
@@ -2,7 +2,6 @@ export default function getRefs() {
     return {
         searchForm: document.querySelector('.search-form'),
         container: document.querySelector('.gallery'),
-        loading: document.querySelector('#loading'),
         btn: document.querySelector('[type="submit"]')
     };
 }
